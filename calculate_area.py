d5d0e00f1bf982ac5cb2de7dd9e60265868b463a
Panda64 <JaylenSchelb@yahoo.com>
2022-02-17 00:36:23 -0800

    Completed exercises

--- calculate_area.py
+++ calculate_area.py
@@ -2,27 +2,27 @@
 # Rename Method
 # Reference: https://parade.com/1039985/marynliles/pick-up-lines/
 
-def cal_un_gr(graph):   # TODO: Rename this function to reflect what it's doing.
+def calc_area(graph):  
     """Calculate the area under the input graph."""
     # bla bla bla.
     pass
 
 #####################
 
-def get_value(li):  # TODO: Rename this function to reflect what it's doing.
-    m = li[0]
+def get_max(li): 
+    max_num = li[0]
     for value in li:
-        if value > m:
-            m = value
-    return m
+        if value > max_num:
+            max_num = value
+    return max_num
 
 
 li = [5, -1, 43, 32, 87, -100]
-print(get_value(li))
+print(get_max(li))
 
 ############################
-def process(sentence):  # TODO: Rename this function to reflect what it's doing.
+def create_word_list(sentence):  # TODO: Rename this function to reflect what it's doing.
     words = sentence[0:].split(' ')
     return words
 
-print(process('If you were a vegetable, you’d be a ‘cute-cumber.'))
+print(create_word_list()('If you were a vegetable, you’d be a ‘cute-cumber.'))
